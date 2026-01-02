ca639082228913d3b1c7246c6901daf0de33d343
Ted Driver <driverte@gmail.com>
2026-01-02 11:56:22 -0700

    1 add initial options for the passwords (#5)
    * Completing initial 2.60 implementation
    
    * Updating Readme
    
    * Updating ci to use just what we need
    
    * Update README.md
    
    * Moving the output creation to the build
    
    Removing it from the CI stage

--- MarkTwain/MarkTwain.cs
+++ MarkTwain/MarkTwain.cs
@@ -5,118 +5,142 @@ using KeePassLib.Security;
 using MarkTwain.Properties;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 // per David, here are more requirements: https://www.xkpasswd.net/
+// Here's how to implement the configuration options: https://claude.ai/chat/e79bea3c-7220-4082-b26b-f2361196bcad
 
 namespace MarkTwain
 {
     public sealed class MarkTwain : CustomPwGenerator
     {
-		private readonly List<string> _passwordList;
-		public MarkTwain()
+        private readonly List<string> _passwordList;
+        public MarkTwain()
         {
-			_passwordList = Resources.WordList.Split(new string[] { "\r\n","\n" },StringSplitOptions.None).ToList();
-        }		
-       
+            _passwordList = Resources.WordList.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).ToList();
+        }
+
         private static readonly PwUuid m_uuid = new PwUuid(new byte[] {
-			0x6A, 0x5E, 0x04, 0xE1, 0xAD, 0xB5, 0x61, 0x41,
+            0x6A, 0x5E, 0x04, 0xE1, 0xAD, 0xB5, 0x61, 0x41,
             0x92, 0x9E, 0xF1, 0x68, 0x80, 0x79, 0xC3, 0xD1 });
-		public override PwUuid Uuid
-		{
-			get { return m_uuid; }
-		}
+        public override PwUuid Uuid
+        {
+            get { return m_uuid; }
+        }
 
         public override string Name => "MarkTwain";
 
-		public string GetWord()
+        public override bool SupportsOptions { get { return true; } }
+
+        public override string GetOptions(string currentOptions)
         {
-			Random c = new Random();
-			var rc = c.Next(0,_passwordList.Count - 1);
-			return _passwordList[rc];
+            using (var dlg = new SettingsForm(currentOptions))
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    var settings = dlg.GetSettings();
+                    //MessageBox.Show(settings, "Password Options", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return settings;
+                }
+            }
+
+            return currentOptions;
+        }
+
+        public string GetWord()
+        {
+            Random c = new Random();
+            var rc = c.Next(0, _passwordList.Count - 1);
+            return _passwordList[rc];
         }
 
         public override ProtectedString Generate(PwProfile prf, CryptoRandomStream crsRandomSource)
         {
-   //         if(prf == null) { Debug.Assert(false); }
-			//else
-			//{
-			//	Debug.Assert(prf.CustomAlgorithmUuid == Convert.ToBase64String(
-			//		m_uuid.UuidBytes, Base64FormattingOptions.None));
-			//}
+            //         if(prf == null) { Debug.Assert(false); }
+            //else
+            //{
+            //	Debug.Assert(prf.CustomAlgorithmUuid == Convert.ToBase64String(
+            //		m_uuid.UuidBytes, Base64FormattingOptions.None));
+            //}
 
-			const int minLength = 14;
-			int maxWords = _passwordList.Count();
-			string pw;
+            MarkTwainOptions options = MarkTwainOptions.Deserialize(prf.CustomAlgorithmOptions);
+
+            int minLength = options.MinimumPasswordLength;
+            int maxWords = _passwordList.Count();
+            string pw;
             do
             {
-				var sb = new StringBuilder();
-                // find indices for two words in the list				
-				ulong u1 = 0;
-				ulong u2 = 0;
-                while (u1 == u2)
+                var sb = new StringBuilder();
+
+                // find words
+                var words = new List<string>();    
+                for (int i = 0; i < options.NumberOfWords; i++)
                 {
-					u1 = crsRandomSource.GetRandomUInt64();
-					u2 = crsRandomSource.GetRandomUInt64();
-					u1 %= (ulong)maxWords;
-					u2 %= (ulong)maxWords;
-                }
-				
-				// get the two words
-				var word1 = _passwordList[(int)u1];
-				var word2 = _passwordList[(int)u2];
-				if(crsRandomSource.GetRandomUInt64() < ulong.MaxValue / 2)
-                {
-					word1 = word1.ToUpperInvariant();
-                }
-                else
-                {
-					word2 = word2.ToUpperInvariant();
+                    var u = crsRandomSource.GetRandomUInt64();
+                    u %= (ulong)maxWords;
+                    var word = _passwordList[(int)u];
+
+                    if(word.Length < options.MinimumWordLength || word.Length > options.MaximumWordLength)
+                    {
+                        i--;
+                        continue;
+                    }
+
+                    words.Add(word);
                 }
 
-				// get indices for special characters
-				ulong sc1 = 0;
-				ulong sc2 = 0;
+                // capitalize one of the words randomly
+                var cap = crsRandomSource.GetRandomUInt64() % (ulong)words.Count;
+                words[(int)cap] = words[(int)cap].ToUpperInvariant();
+                
+                // get indices for special characters
+                ulong sc1 = 0;
+                ulong sc2 = 0;
                 while (sc1 == sc2)
                 {
-					sc1 = crsRandomSource.GetRandomUInt64();
-					sc2 = crsRandomSource.GetRandomUInt64();
-					sc1 %= (ulong)PwCharSet.PrintableAsciiSpecial.Length;
-					sc2 %= (ulong)PwCharSet.PrintableAsciiSpecial.Length;
+                    sc1 = crsRandomSource.GetRandomUInt64();
+                    sc2 = crsRandomSource.GetRandomUInt64();
+                    sc1 %= (ulong)PwCharSet.PrintableAsciiSpecial.Length;
+                    sc2 %= (ulong)PwCharSet.PrintableAsciiSpecial.Length;
                 }
 
-				var specialCharacter1 = PwCharSet.PrintableAsciiSpecial[(int)sc1];
-				var specialCharacter2 = PwCharSet.PrintableAsciiSpecial[(int)sc2];
+                var specialCharacter1 = PwCharSet.PrintableAsciiSpecial[(int)sc1];
+                var specialCharacter2 = PwCharSet.PrintableAsciiSpecial[(int)sc2];
 
-				// get indices for digits
-				int number1 = 0;
-				int number2 = 0;
-				while(number1 == number2)
+                // get indices for digits
+                int number1 = 0;
+                int number2 = 0;
+                while (number1 == number2)
                 {
-					var rb1 = crsRandomSource.GetRandomBytes(2);
-					var rb2 = crsRandomSource.GetRandomBytes(2);
-					number1 = BitConverter.ToUInt16(rb1,0)%99;
-					number2 = BitConverter.ToUInt16(rb2,0)%99;
+                    var rb1 = crsRandomSource.GetRandomBytes(2);
+                    var rb2 = crsRandomSource.GetRandomBytes(2);
+                    number1 = BitConverter.ToUInt16(rb1, 0) % 99;
+                    number2 = BitConverter.ToUInt16(rb2, 0) % 99;
                 }
-				// create the password
-				sb.Append(specialCharacter1.ToString() + 
-						  specialCharacter1.ToString() +
-						  number1.ToString() +
-						  specialCharacter2.ToString() +
-						  word1 +
-						  specialCharacter2.ToString() +
-						  word2 +
-						  specialCharacter2.ToString() +
-						  number2.ToString() +
-						  specialCharacter1.ToString() +
-						  specialCharacter1.ToString());
-				pw = sb.ToString();
-            }while(pw.Length < minLength);
 
-			return new ProtectedString(false,pw);
+                // create the password
+                sb.Append(specialCharacter1.ToString() +
+                        specialCharacter1.ToString() +
+                        number1.ToString());
+
+                foreach (var w in words)
+                {
+                    sb.Append(specialCharacter2.ToString() + w);
+                }
+               
+                sb.Append(
+                specialCharacter2.ToString() +
+                number2.ToString() +
+                specialCharacter1.ToString() +
+                specialCharacter1.ToString());
+
+                pw = sb.ToString();
+            } while (pw.Length < minLength);
+
+            return new ProtectedString(false, pw);
         }
     }
 }
---
+++ MarkTwain/SettingsForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace MarkTwain
+{
+    public partial class SettingsForm : Form
+    {
+        MarkTwainOptions options = new MarkTwainOptions();
+
+        public SettingsForm(string optionString)
+        {
+            InitializeComponent();
+            try
+            {
+                var opts = MarkTwainOptions.Deserialize(optionString);
+                nudMinPasswordLength.Value = opts.MinimumPasswordLength;
+                nudNumWords.Value = opts.NumberOfWords;
+                nudMinLength.Value = opts.MinimumWordLength;
+                nudMaxLength.Value = opts.MaximumWordLength;
+            }
+            catch
+            {
+                nudMinPasswordLength.Value = 8;
+                nudNumWords.Value = 2;
+                nudMinLength.Value = 3;
+                nudMaxLength.Value = 10;
+            }
+            ;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            options.MinimumPasswordLength = (int)nudMinPasswordLength.Value;
+            options.NumberOfWords = (int)nudNumWords.Value;
+            options.MinimumWordLength = (int)nudMinLength.Value;
+            options.MaximumWordLength = (int)nudMaxLength.Value;
+            this.DialogResult = DialogResult.OK;
+        }
+
+        public string GetSettings()
+        {
+            return options.Serialize();
+        }
+
+    }
+}
